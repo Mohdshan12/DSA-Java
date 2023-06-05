09efad19238b1e7c1f7452cb8a99d2dc6dce7389
mohdshanshekh <mohdshanshekh@gmail.com>
2023-06-05 23:38:47 +0530

    first programe1

--- ifElsePractice.java
+++ ifElsePractice.java
@@ -1,6 +1,7 @@
 public class ifElsePractice {
     public static void main(String[] args) {
         int n  = 5;
+
         if (n > 5) {
             System.out.println("Greater than 5");
         }
