5a70a91c722c13180133c2c6665233697cbc00d1
mohdshanshekh <mohdshanshekh@gmail.com>
2023-06-05 23:29:17 +0530

    first programe

---
+++ ThingsToKnowJava.java
@@ -0,0 +1,5 @@
+public class ThingsToKnowJava {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ ifElsePractice.java
@@ -0,0 +1,11 @@
+public class ifElsePractice {
+    public static void main(String[] args) {
+        int n  = 5;
+        if (n > 5) {
+            System.out.println("Greater than 5");
+        }
+        else {
+            System.out.println("less than or equal 5");
+        }
+    }
+}
