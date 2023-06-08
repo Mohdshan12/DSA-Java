7ec5d08b685d987e2544ec154fc84e4f0d4a3497
mohdshanshekh <mohdshanshekh@gmail.com>
2023-06-08 23:09:18 +0530

    Pattren Day 3

--- PattrenPrograme/Pattren11.java
+++ PattrenPrograme/Pattren11.java
@@ -12,8 +12,15 @@ public class Pattren11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
-
+        for(int row = 0;row < n;row++) {
+            for(int col = 0;col < row;col++) {
+                System.out.print(" ");
+            }
+            for(int col = 0;col < n-row;col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
   
     }
     
