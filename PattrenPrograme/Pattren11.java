18b5f62b4ef95a93f29a1553a355f08995c1a126
mohdshanshekh <mohdshanshekh@gmail.com>
2023-06-08 22:57:59 +0530

    master

---
+++ PattrenPrograme/Pattren11.java
@@ -0,0 +1,20 @@
+/*
+ *   * * * * *
+      * * * *
+       * * *
+        * *
+         *
+    
+ */
+package PattrenPrograme;
+import java.util.Scanner;
+public class Pattren11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+
+
+    }
+    
+}
