3e26eebb79d485fc307d5897b0d07a266544236f
mohdshanshekh <mohdshanshekh@gmail.com>
2023-06-08 22:59:27 +0530

    demo

--- PattrenPrograme/Pattren11.java
+++ PattrenPrograme/Pattren11.java
@@ -12,9 +12,9 @@ public class Pattren11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        
 
 
+  
     }
     
 }
